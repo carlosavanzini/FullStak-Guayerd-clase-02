0507d1c0be29da690e67b187e8dde683dd9a5ef5
carlosavanzini <avanzinidesarrollo@gmail.com>
2021-06-17 21:35:23 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Algoritmo “Adiviná el número”
+// 1 - Elegir y guardar un número al azar (entre 1 y 100) 
+// 2 - Pedirle al usuario que ingrese un número 
+// 3 - Sumar uno en cantidad de intentos 
+// 4 - Si el número ingresado es igual al pensado 
+// 	Informar “Ganaste” y Terminar 
+// Si el número ingresado es mayor al pensado
+// 	Informar “Es mayor”
+// Si el número ingresado es menor al pensado
+// 	Informar “Es menor”
+// 5 - Volver al paso 2 
+
+// Terminar:
+// 1 - Informar: Ganaste! Cantidad de intentos: XX
+
+// Extra: Guardar todos los intentos, verificar el ingreso para que el usuario no repita un número.
+// Extra 2: Mostrar en pantalla los números ingresados.
+
+
 //asignamos a la variable numero un numero randon 
 let numeroRandom = Math.round(Math.random()*100);
 
@@ -5,10 +24,21 @@ let numeroRandom = Math.round(Math.random()*100);
 let intentos = 0;
 //declaro varible
 let numeroUsuario;
-
+let repetido=[];
+let esta=0;
 // codigo
 do {
+   
     numeroUsuario = parseInt(prompt("¿Por favor ingrese un numero entre 1 y 100?"));
+    for (let i = 0; i <repetido.length; i++) {
+        if(numeroUsuario===repetido[i]){
+            alert(`Numero ya ingresado`)
+        }
+        
+    }  
+    repetido.push(numeroUsuario)
+   
+   
     if (numeroUsuario === numeroRandom) {
         alert(`Ganaste Felicidades!!!! el numero random elegido por la maquina era el ${numeroUsuario}`)
     } else {
